04fd5f985f8cce88d4988063ea829bb8593b2822
Martin Wegmann <martin.wegmann@remote-sensing-biodiversity.org>
2014-05-08 19:46:11 +0200

    first indices and their formula in a list

--- R/spectralIndices.R
+++ R/spectralIndices.R
@@ -10,10 +10,25 @@
 #' @export
 spectralIndices <- function(inputRaster, index, filename = NULL, maskRaster = NULL, verbose = FALSE, overwrite = TRUE, ...) {
   # TODO: add indices
-  # TODO: how to define indices with >3 bands?
+  # TODO: how to define indices with >3 bands? e.g. slavi = NIR / (RED + MIR) or EVI
   # TODO: add examples
+  # required packages: raster   
+  
+  
+  listOfIndices <-  list(
+                          ratio = (NIR/RED),
+                          dvi = NIR-RED,
+                          ndvi = (NIR-RED)/(NIR+RED), 
+                          tvi = (((NIR-RED)/(NIR+RED))+0.5)^0.5, 
+                          msavi = NIR + 0.5 - (0.5 * sqrt((2 * NIR + 1)^2 - 8 * (NIR - (2 * Red)))),
+                          msavi2 = (2 * (NIR + 1) - sqrt((2 * NIR + 1)^2 - 8 * (NIR - Red))) / 2,
+                          gemi = (((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * (1 - ((((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * 0.25)) - ((RED - 0.125) / (1 - RED))                   
+                          
+                          )
+                                               
+                                                 
+                         
+                          
 
-  
-  
-  
+
   
