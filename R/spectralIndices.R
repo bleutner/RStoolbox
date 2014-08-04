158fc2969d0382b32c635f617de2b69ecd5bea46
bleutner <benjamin.leutner@uni-wuerzburg.de>
2014-08-04 19:18:29 +0200

    spectralIndices now working

--- R/spectralIndices.R
+++ R/spectralIndices.R
@@ -8,29 +8,45 @@
 #' @return the raster holding the index results  
 #' @seealso \code{\link{...}} 
 #' @export
-spectralIndices <- function(inputRaster, index, filename = NULL, maskRaster = NULL, verbose = FALSE, overwrite = TRUE, ...) {
-  # TODO: add indices
-  # TODO: how to define indices with >3 bands? e.g. slavi = NIR / (RED + MIR) or EVI
-  # TODO: add examples
-  # required packages: raster   
-  
-  
-  RedNirIndices <-  list(
-                          ratio = (NIR/RED),
-                          dvi = NIR-RED,
-                          ndvi = (NIR-RED)/(NIR+RED), 
-                          tvi = (((NIR-RED)/(NIR+RED))+0.5)^0.5, 
-                          msavi = NIR + 0.5 - (0.5 * sqrt((2 * NIR + 1)^2 - 8 * (NIR - (2 * Red)))),
-                          msavi2 = (2 * (NIR + 1) - sqrt((2 * NIR + 1)^2 - 8 * (NIR - Red))) / 2,
-                          gemi = (((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * (1 - ((((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * 0.25)) - ((RED - 0.125) / (1 - RED))                   
-                          
-                          )
-                                               
-                                                 
-                         
-  ThreeBandIndices <- list(
-                          slavi = NIR / (RED + MIR),
-                          EVI = G * ((NIR - RED) / (NIR + C1 * RED - C2 * BLUE + L))# include a G or L specification in command
-                  
-                          )
-					  }
+#' @example
+#' 
+spectralIndices <- function(inputRaster, indices = "NDVI", sensor, bands = list(), filename = NULL, maskRaster = NULL, verbose = FALSE, overwrite = TRUE, ...) {
+	# TODO: add indices
+	# TODO: add examples
+	# TODO: add formulas to help file
+	# TODO: internal sensor db
+	# TODO: value checks?
+	bands <- lapply(bands, function(x) if(is.character(x)) which(names(inputRaster) == x) else x )
+	
+	## Internal db
+	INDICES <-  list(
+			SR 		= function(NIR, RED) {NIR / RED},
+			DVI		= function(NIR, RED) NIR-RED,
+			NDVI	= function(NIR, RED) (NIR-RED)/(NIR+RED), 
+			TVI 	= function(NIR, RED) (((NIR-RED)/(NIR+RED))+0.5)^0.5, 
+			MSAVI	= function(NIR, RED) NIR + 0.5 - (0.5 * sqrt((2 * NIR + 1)^2 - 8 * (NIR - (2 * RED)))),
+			MSAVI2	= function(NIR, RED) (2 * (NIR + 1) - sqrt((2 * NIR + 1)^2 - 8 * (NIR - RED))) / 2,
+			GEMI	= function(NIR, RED) (((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * (1 - ((((NIR^2 - RED^2) * 2 + (NIR * 1.5) + (RED * 0.5) ) / (NIR + RED + 0.5)) * 0.25)) - ((RED - 0.125) / (1 - RED)),                   
+			SLAVI	= function(RED, MIR) NIR / (RED + MIR),
+			EVI		= function(NIR, RED, BLUE) G * ((NIR - RED) / (NIR + C1 * RED - C2 * BLUE + L))# include a G or L specification in command
+	)
+	
+	## Get arguments and check for mising arguments
+	args <- lapply(indices, function(index) {
+				need <- names(formals(INDICES[[index]]))	
+				if(any(!names(bands) %in% need)) stop("Band specification(s) of >> ", paste(names(bands)[!names(bands) %in% need], collapse = ","), 
+							" << are missing or do not match layer names in the brick/stack. \nPlease specify the correct layer number or name in a list, e.g. bands = list(RED = 'B4', NIR = 'B5')", call. = FALSE)
+				need <- unlist(bands[need])
+			})
+	names(args) <- indices
+	
+	## We do this in a separate step, so we can throw an error before if there is one
+	inlist <- lapply(indices, function(index) {
+				overlay(inputRaster[[args[[index]]]], fun = INDICES[[index]])
+			})
+	
+	## Combine and return
+	outlist <- stack(inlist)
+	names(outlist) <- indices	
+	return(outlist)
+}
