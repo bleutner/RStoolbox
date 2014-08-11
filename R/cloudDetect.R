bb5cae6c889bb6d22fa0a53d7ee4a051106cb52a
bleutner <benjamin.leutner@uni-wuerzburg.de>
2014-08-11 19:03:11 +0200

    added (semi-)automatic cloud masking function cloudDetect()

---
+++ R/cloudDetect.R
@@ -0,0 +1,93 @@
+#' Very simple cloud detection for imagery with blue and thermal bands
+#' 
+#' @param x RasterBrick or RasterStack
+#' @param threshold cloud detection threshold. If not provided it will be guessed.
+#' @param minCloudSize minimum number of cloud pixels in window1 
+#' @param windowSize1 odd number, rectangular moving window to remove clouds which arre too small (likely artefacts)
+#' @param windowSize2 odd number, rectangular buffer around cluster centers
+#' @param sanitize logical. Should small clouds (possibly false positives) be removed by filtering? If \code{TRUE} windowSize1 must be specified.
+#' @param maskGrowing logical. Applies simple region-growing (rectangular buffering) to the cloud mask. If \code{TRUE} windowSize2 must be specified.
+#' @param lowBand bandname or number for the blue band
+#' @param tirBand bandname or number for the thermal band
+#' @param plot logical. Provides plots of the cloud mask for all sub-steps (sanitizing etc.) Helpful to find proper parametrization.
+#' @param verbose logical. Print messages or supress.
+#' @param returnDiffLayer logical. If \code{TRUE}, the difference layer will be returned along with the cloudmask. This option allows to re-use the difference layer in cloudDetect.
+#' @note Typically clouds are cold in the thermal (high value) and have high reflectance in short wavelengths. By ratioing the two bands an rough cloud mask can be obtained.
+#' More precise approaches can be found elsewhere, e.g. \link[fmask]{https://code.google.com/p/fmask/}.
+#' @export
+#' @examples 
+#' \dontrun{
+#' ls <- stackMeta("path/to/MTL.txt")
+#' ls_cor <- radCor(ls, "path/to/MTL.txt") 
+#' ls_cmask <-cloudDetect(ls_cor, returnDiffLayer = TRUE)
+#' }
+cloudDetect <- function(x, threshold, minCloudSize, windowSize1 = 5, windowSize2 = 11, maskGrowing = TRUE, sanitize = TRUE, lowBand = "B1", tirBand = "B6", plot = TRUE, verbose = TRUE, returnDiffLayer = FALSE){
+	
+	## Set-up graphics device
+	op <- par(mfrow = c(2, 1 + sum(sanitize, maskGrowing)))
+	
+	## Calculate or re-reuse cloud difference layer	
+	if("CDIFF" %in% names(x)) {
+		if(verbose) message("Re-using CDIFF layer from previous run.")
+		cdiff <- x[["CDIFF"]]
+	} else {
+		cdiff <- x[[lowBand]] - x[[tirBand]]
+		names(cdiff) <- "CDIFF"
+	}
+	
+	## Guess threshold
+	if(missing(threshold)) {
+		threshold <- quantile(cdiff@data@max:cdiff@data@min, 0.45)
+		if(verbose) {message(paste0("Estimated cloud threshold should be between ", round(cdiff@data@min), " and ", round(cdiff@data@max)) )
+			message(paste0("Guessed threshold (rounded): ", round(threshold)))
+		}
+	}
+	if(threshold < cdiff@data@min | threshold > cdiff@data@max) warning("Threshold is not within the estimated data range", call. = FALSE)
+	
+	if(plot) plot(cdiff, main = "Cloud layer: blue - tir difference")
+	
+	## Thresholding
+	cmask <- cdiff > threshold
+	cmask <- mask(cmask, cmask, maskvalue = 0)
+	
+	if(plot) plot(cmask, main = paste0("Cloud mask\nThreshold: ", threshold))
+	
+	
+	## Remove "clouds" smaller than minCloudSize
+	if(sanitize) {
+		if(missing(minCloudSize)) minCloudSize <- windowSize1 ^ 2
+		w <- matrix(ncol = windowSize1, nrow = windowSize1, 1)
+		if(minCloudSize >= windowSize^2) {
+			cmod <- focal(cmask, w, na.rm = FALSE)
+		} else {
+			cmod <- focal(cmask, w, na.rm = TRUE)	
+			cmod[cmod < minCloudSize] <- NA		
+		}
+		cmod[cmod < minCloudSize] <- NA
+		cmod[!is.na(cmod)] <- 1L
+		if(plot) plot(cmod, main = "Sanitized cloud mask")
+		
+	}
+	
+	## Buffer cloud centers (we could also do a circular buffer, but for now this should suffice)
+	if(maskGrowing){
+		w <- matrix(ncol = windowSize2, nrow = windowSize2, 1)
+		cmod <- focal(cmod, w, na.rm = TRUE )
+		cmod[!is.na(cmod)] <- 1L
+		if(plot) plot(cmod, main = "Region-grown cloud mask")
+		
+	}
+	
+	if(plot){
+		plotRGB(x, 1, 2, 3, title = "Final mask", stretch = "lin")
+		plot(cmod,  legend = FALSE, add = T, col = "yellow")
+	}
+	
+	## Reset par
+	par(op)
+	
+	## Return
+	names(cmod) <- "CMASK"
+	if(returnDiffLayer) cmod <- stack(cmod, cdiff)
+	return(cmod)	
+}
