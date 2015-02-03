bcca0993b2dad1c9c86842bf151f339ae5d7c72c
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-02-03 17:11:56 +0100

    saveRSTBX and readRSTBX added for archiving classification results including models

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -5,8 +5,8 @@
 #' @param adate character. date in format "YYYY-MM-DD" 
 #' @keywords internal
 .ESdist <- function(adate){	
-	edist <- julian(as.Date(adate), origin=as.Date(paste(substring(adate, 1, 4), "12", "31", sep="-")))[[1]]
-	 1 - 0.016729 * cos((2*pi) * (0.9856 * (edist - 4)/360))
+    edist <- julian(as.Date(adate), origin=as.Date(paste(substring(adate, 1, 4), "12", "31", sep="-")))[[1]]
+    1 - 0.016729 * cos((2*pi) * (0.9856 * (edist - 4)/360))
 }
 
 
@@ -17,11 +17,11 @@
 #' @note decimal numbers will be returned as two separate numbers
 #' @keywords internal
 .getNumeric <- function(x, returnNumeric = TRUE) {
-	sapply(x, function(xi){
-				d <- strsplit(xi, "[^[:digit:]]")[[1]]
-				d <- if(returnNumeric) as.numeric(d[d!=""]) else d[d!=""]
-				d
-			})
+    sapply(x, function(xi){
+                d <- strsplit(xi, "[^[:digit:]]")[[1]]
+                d <- if(returnNumeric) as.numeric(d[d!=""]) else d[d!=""]
+                d
+            })
 }
 
 
@@ -48,4 +48,41 @@
 }
 
 
+#' Get file extension for writeRaster
+#' @param x character. Format driver.
+#' @keywords internal
+.rasterExtension <- function(x){ 
+    fdb <- c(RASTER = ".grd", GTIFF = ".tif", CDF = ".nc", KML = ".kml", KMZ = ".kmz", BIG.MATRIX = ".big",
+            BIL = ".bil", BSQ = ".bsq", BIP = ".bip",ASCII = ".asc", RST = ".rst",ILWIS = ".mpr", 
+            SAGA = ".sdat",BMP = ".bmp", ADRG = ".gen", BT = ".bt", EHDR = ".bil",ENVI = ".envi",
+            ERS = ".ers", GSBG = ".grd",HFA =  ".img", IDA =  ".img", RMF = ".rsw")
+    ext <- fdb[toupper(x)]
+    if(is.na(ext)) ".grd" else ext
+}
+
+#' Expand any filepath to the full path
+#' @param x character. Path.
+#' @keywords internal   
+#' @examples 
+#' .fullPath("test.grd")
+#' .fullPath("../test.grd") 
+#' .fullPath("../../../../../test.grd")
+#' .fullPath("~/test.grd") 
+#' .fullPath("/tmp/test.grd")
+.fullPath <- function(x){
+    x <- path.expand(x)   
+    x <- gsub("\\\\", "/", x) ## anti-win
+    x <- gsub("//", "/", x)   ## anti-win
+    
+    if(basename(x) == x | grepl("^[.][.]/", x))    x   <- file.path(getwd(),x)
+    if(grepl("[.][.]", x)){
+        xs  <- str_split(x, "/")[[1]]
+        ups <- grep("[.][.]", xs)  
+        rem <- c(ups, ups-length(ups))
+        rem <- rem[rem > 1]
+        xs  <-  xs[-rem]
+        x   <- paste0(xs,collapse="/")      
+    }
+    x           
+}   
 
