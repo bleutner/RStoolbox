91e328f0013b359a870c9078f385e86f5d1301b8
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-02-05 19:23:23 +0100

    adding references and more indices

--- R/spectralIndices.R
+++ R/spectralIndices.R
@@ -22,7 +22,7 @@
 #' SI <- spectralIndices(r, red = 1, nir = 2, indices = "NDVI")
 #' plot(SI)
 spectralIndices <- function(inputRaster, blue=NULL, red=NULL, nir=NULL, mir=NULL, indices=NULL, coefs = list(L = 0.5,  G = 2.5, L_evi = 1,  C1 = 6,  C2 = 7.5),
-                ... ) {
+        ... ) {
     # TODO: add further indices
     
     ## Coefficients
@@ -36,7 +36,7 @@ spectralIndices <- function(inputRaster, blue=NULL, red=NULL, nir=NULL, mir=NULL
     ind <- if(is.null(indices)) names(.IDXdb) else toupper(indices)  
     if(!any(ind %in% names(.IDXdb))) stop("indices must either be NULL to calculate all indices",
                 "\nor element of c(", paste0(names(.IDXdb),collapse=","),") for specific indices.", call. = FALSE)
-        
+    
     ## Gather function arguments (all provided bands) and create args
     args    <- alist(blue=, red=, nir=, mir=)
     potArgs <- names(args) ## potential bands
@@ -81,7 +81,7 @@ spectralIndices <- function(inputRaster, blue=NULL, red=NULL, nir=NULL, mir=NULL
     # this should save some significant time for large Rasters   
     indexMagic    <- overlay(inputRaster[[bands]], fun = funMaster, ...)
     names(indexMagic) <- names(bdys)      
-        
+    
     return(indexMagic)
 }
 
@@ -90,16 +90,43 @@ spectralIndices <- function(inputRaster, blue=NULL, red=NULL, nir=NULL, mir=NULL
 #' Database of spectral indices
 #' @keywords internal
 .IDXdb <-  list(               
-        DVI		= function(red, nir) {nir-red},
+        DVI		= function(red, nir) {s*nir-red},
         EVI		= function(red, nir, blue) {G * ((nir - red) / (nir + C1 * red - C2 * blue + L_evi))},
-        GEMI	= function(red, nir) {(((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * (1 - ((((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * 0.25)) - ((red - 0.125) / (1 - red))},                   
+        GEMI	= function(red, nir) {(((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * (1 - ((((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * 0.25)) - ((red - 0.125) / (1 - red))},
+        LSWI	= function(nir, swir) {(nir-swir)/(nir+swir)},
         MSAVI	= function(red, nir) {nir + 0.5 - (0.5 * sqrt((2 * nir + 1)^2 - 8 * (nir - (2 * red))))},
         MSAVI2	= function(red, nir) {(2 * (nir + 1) - sqrt((2 * nir + 1)^2 - 8 * (nir - red))) / 2},
         NDVI	= function(red, nir) {(nir-red)/(nir+red)}, 
+        NDWI 	= function(green, nir) {(green - nir)/(green + nir)},
         SAVI    = function(red, nir) {(nir - red) * (1+L) / (nir + red + L)}, 
         SLAVI	= function(red, nir, mir) {nir / (red + mir)},
         SR 		= function(red, nir) {nir / red},     
-        TVI 	= function(red, nir) {(((nir-red)/(nir+red))+0.5)^0.5}      
+        TVI 	= function(red, nir) {sqrt(((nir-red)/(nir+red))+0.5)},
+   #     CTVI	= function(red, nir) {}
+        WDVI    = function(red, nir) {nir - s * red}
 )
+
+#' References and names to .IDXdb
+#' @keywords internal
+.IDX.REFdb <- list(
+        DVI     = c("Richardson1977", "Difference Vegetation Index") ,
+        EVI     = c("Huete1999", "Enhanced Vegetation Index"),
+        GEMI    = c("Pinty1992","Global Environmental Monitoring Index"),
+        LSWI    = c("", "Land Surface Water Index"),
+        MSAVI	= c("Qi1994","Modified Soil Adjusted Vegetation Index"),
+        MSAVI2	= c("Qi1994","Modified Soil Adjusted Vegetation Index 2"),
+        NDVI	= c("Rouse1974", "Normalised Difference Vegetation Index"),
+        NDWI	= c("", "Normalised Difference Water Index"),
+        SAVI    = c("Huete1988", "Soil Adjusted Vegetation Index"),
+        SLAVI	= c("Lymburger2000","Specific Leaf Area Vegetation Index"),
+        SR 		= c("Birth1968", "Simple Ratio Vegetation Index"),  #or Jordan1969
+        TVI 	= c("Deering1975","Transformed Vegetation Index"),
+        WDVI    = c("Richardson1977","Weighted Difference Vegetation Index")
+)
+
+
+
+
+
 
 
