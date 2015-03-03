339700a8ad05c63745a28d2c45391a6c67f83a50
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-03-03 16:48:04 +0100

    finished panSharpen

--- R/panSharp.R
+++ R/panSharp.R
@@ -1,9 +1,24 @@
-
-panSharp <- function(img, pan, method = "pca", norm=TRUE) {
-    if(method == "simple") {
-        imgHiRes <- raster::resample(img, pan, method = "ngb")
-        out <- .paraRasterFun(imgHiRes, rasterFun=overlay, args = list(y = pan, fun ="/"))
-    }
+#' Pan sharpen imagery / Image fusion
+#' 
+#' provides different methods for pan sharpening a coarse resolution (typically multispectral) image with 
+#' a higher reolution panchromatic image
+#' 
+#' @param img Raster* object. Coarse resolution imagery 
+#' @param pan RasterLayer. High resolution image, typically panchromatic.
+#' @param method Character. Choose method from c("pca", "ihs").
+#' @param r Character or Integer. Red band in \code{img}. Only relevant if \code{method='pca'}
+#' @param g Character or Integer. Green band in \code{img}. Only relevant if \code{method='pca'}
+#' @param b Character or Integer. Blue band in \code{img}. Only relevant if \code{method='pca'}
+#' @param norm Logical. Normalize pan image to 1st PC component. If \code{FALSE} pan will be histogram matched to the 1st PC. Otherwise only min and max are matched.
+#' @details 
+#' Pan sharpening options:
+#' \itemize{ 
+#'  \item{\code{method='pca'}: Performs a pca using \link{rasterPCA}. The first component is then swapped for the pan band an the PCA is rotated backwards.}
+#'  \item{\code{method='ihs'}: Performs a color space transform to Intensity-Hue-Saturation space, swaps intensity for the histogram matched pan and does the backwards transformation.}
+#' }
+#' @export
+panSharpen <- function(img, pan, r, g, b, method = "pca", norm=TRUE) {
+  
     if(method == "pca") {
         imgpca <- rasterPCA(img)
         
@@ -21,77 +36,29 @@ panSharp <- function(img, pan, method = "pca", norm=TRUE) {
         
         names(panimg) <- paste0(names(img), "_pan")
         return(panimg)
-    } else {
-        img <- normImage(img, xmin = 0, xmax = 2^16/2, ymin=0, ymax=1)
-        imgSum <- .paraRasterFun(img[[c(r,g,b)]],rasterFun = calc, args = list(fun = sum))
-        imgNorm <- .paraRasterFun(img[[c(r,g,b)]], rasterFun = overlay, args = list(y = imgSum, fun = "/"))
-        .H <- function(r, g, b) { 
-            out <- acos((r - g/2 - b/2) /  sqrt((r-g)^2 + (r-b)*(g-b))) 
-            m1 <- b > g
-            out[m1] <- 2*pi - out[m1] 
-            out           
-        }
-        H <- .paraRasterFun(imgNorm, rasterFun = overlay, args = list(fun =.H))
-        # H <- H * 180/pi
-        S <- 1 - 3 * .paraRasterFun(imgNorm, rasterFun = calc, args=list(fun=min))
-        I <- imgSum / 3    
-        
-        ## reverse
-        hsi2rgb <- function(H, S, I){
-            rr1 <- numeric(length(H))
-            case1 <- H < 2*pi/3
-            case3 <- H >= 4*pi/3
-            case2 <- !(case1 | case3)
-            
-            rr1[case1] <-  1/3 * (1 + (S[case1] * cos(H[case1]))/cos(pi/3-H[case1]))
-            rr1[case2] <-  1/3 * (1 + (S[case2] * cos(H[case2] - 2*pi/3))/cos(pi-H[case2]))
-            rr1[case3] <-  1/3 * (1 + (S[case3] * cos(H[case3] - 4*pi/3))/cos(5*pi/3-H[case3]))
-            
-            rr2 <- (1 - S) / 3
-            rr3 <-  1 - rr2 - rr1
-            
-            out <- matrix(ncol=3, nrow=length(H))
-            colnames(out) <- c("r", "g", "b")
-            
-            out[case1, ] <- cbind(rr1[case1], rr3[case1], rr2[case1])
-            out[case2, ] <- cbind(rr2[case2], rr1[case2], rr3[case2])
-            out[case3, ] <- cbind(rr3[case3], rr2[case3], rr1[case3])
-            
-            return(out)         
-        }
-        
-        
-        rgb <- raster::overlay(stack(H,S,I), fun = hsi2rgb)
-        
-        hsv <- overlay(img[[c(r,g,b)]], fun = rgb2hsv)
-        rgbb <- overlay(hsv, fun=function(x,y,z) t(col2rgb(hsv(x,y,z))))    
-        
-        
-        HSV <- overlay(imgNorm, fun=rgb2hsv, max = 32767)
-        
-        M <-  matrix(c(rep(1/3,3), rep(sqrt(6)^-1,2), -2/sqrt(6), 1/sqrt(2), -1/sqrt(2), 0), ncol=3, byrow = T)
-        Mr <- t(M)
-        Mr[,1] <- 1 
-        
-        Ivv <- M %*% t(img[[c(r,g,b)]][1:10])
-        Ivv <- calc(img[[c(r,g,b)]], fun = function(x) t(M %*% t(x)))
-        
-        Ivvr <- raster::resample(Ivv[[2:3]], pan, method = "bilinear")
-        panMa <- histMatch(pan, Ivv[[1]])
-        RGB <- calc(stack(panMa, Ivvr), fun = function(x) t(Mr %*% t(x)))
-        
-        RGB <- t(Mr %*% Ivv)
-        plot(imgSum)
     }
-    
-}            
+    if(method == "ihs")     {  
+        #xmax <- .DATATYPEdb[dataType(img[[c(r,g,b)]]),"max"]
+        #img <- normImage(img[[c(r,g,b)]], xmin = 0, xmax = xmax, ymin=0, ymax=1)          
+        
+        Mfwd <- t(matrix(c(rep(1/3,3), rep(sqrt(6)^-1,2), -2/sqrt(6), 1/sqrt(2), -1/sqrt(2), 0), ncol=3, byrow = T))   ## Carper1990
+        Mbwd <- t(Mfwd)
+        Mbwd[1,] <- 1  
+        
+        Ivv   <- .paraRasterFun(img[[c(r,g,b)]], rasterFun = calc, args = list( fun = function(x) x %*% Mfwd))
+        Ivvr  <- raster::resample(Ivv[[2:3]], pan, method = "bilinear")
+        panMa <- histMatch(pan, Ivv[[1]])
+        panimg   <- .paraRasterFun(stack(panMa, Ivvr) , rasterFun = calc, args = list( fun = function(x) x %*% Mbwd))
+        return(panimg)
+    }
+}
+
+
 
 
 normImage <- function(x, y, xmin, xmax, ymin, ymax, forceMinMax = FALSE) {
-    if(forceMinMax) {
-        x <- setMinMax(x)
-        if(!missing(y))  y <- setMinMax(y)
-    }
+    if(forceMinMax)  x <- setMinMax(x)
+    if(!missing(y) && forceMinMax)  y <- setMinMax(y)
     if(missing("ymin")) ymin <- y@data@min
     if(missing("ymax")) ymax <- y@data@max
     if(missing("xmin"))	xmin <- x@data@min 
