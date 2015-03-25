50e1a4f7905bb2cf6b083f006398952d83ea372d
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-03-25 11:54:40 +0100

    check file exists

--- R/readMeta.R
+++ R/readMeta.R
@@ -8,6 +8,7 @@
 #' @export 
 #' 
 readMeta <- function(file, raw = FALSE){
+    if(!file.exists(file)) stop("File does not exist.")
     if(!grepl("MTL", file) & !grepl("xml", file)) warning("The Landsat metadata file you have specified looks unusual. Typically the filename contains the string 'MTL' or 'xml'. Are you sure you specified the right file? \n I'll try to read it but check the results!")
     
     format <- if(grepl('xml', file)) "XML" else "MTL"
