c6fdfab57c0a1a2979b264099dd84e4b2f53af9e
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-07-09 19:16:23 +0200

    unsuperClass fixes

--- R/unsuperClass.R
+++ R/unsuperClass.R
@@ -5,12 +5,13 @@
 #' Clustering is done using stats::kmeans. This can be done for all pixels of the image (clusterMap=FALSE), however this can be slow and is
 #' not memory safe. Therefore if you have large raster data (> memory), as is typically the case with remote sensing imagery it is adviseable to choose clusterMap=TRUE (the default).
 #' This means that a kmeans cluster map is calculated based on a random subset of pixels (nSamples). Then the distance of *all* pixels to the cluster centers 
-#' is calculated in a stepwise fashion using raster::predict. Class assignment is based on minimum distance to the cluster centers.   
+#' is calculated in a stepwise fashion using raster::predict. Class assignment is based on minimum euclidean distance to the cluster centers.   
 #' 
 #' @param img Raster* object. 
 #' @param nSamples integer. Number of random samples to draw to fit cluster map. Only relevant if clusterMap = FALSE.
 #' @param nClasses integer. Number of classes.
 #' @param nStarts  integer. Number of random starts for kmeans algorithm.
+#' @param norm Logical. If \code{TRUE} will normalize img first using \link{normImage}. Normalizing is beneficial if your predictors have different scales.
 #' @param clusterMap logical. Fit kmeans model to a random subset of the img (see Details).
 #' @param algorithm character. \link[stats]{kmeans} algorithm. One of c("Hartigan-Wong", "Lloyd", "MacQueen")
 #' @param ... further arguments to be passed to \link[raster]{writeRaster}, e.g. filename
@@ -36,16 +37,16 @@
 #'        title = "Classes", horiz = TRUE,  bty = "n")
 #' 
 #' par(olpar) # reset par
-unsuperClass <- function(img, nSamples = 1000, nClasses = 5, nStarts = 25, clusterMap = TRUE, algorithm = "Hartigan-Wong", ...){      
+unsuperClass <- function(img, nSamples = 10000, nClasses = 5, nStarts = 25, norm = FALSE, clusterMap = TRUE, algorithm = "Hartigan-Wong", ...){      
     if(atMax <- nSamples > ncell(img)) nSamples <- ncell(img)
     wrArgs <- list(...)
-   
-    if(!clusterMap | atMax && canProcessInMemory(img, n = 2)){
+    if(norm) img <- normImage(img)
+    if(!clusterMap | atMax && canProcessInMemory(img, n = 4)){
         trainData <- img[]
         complete  <- complete.cases(trainData)
         model     <- kmeans(trainData[complete,], centers = nClasses, nstart = nStarts, algorithm = algorithm)
-        out   <- raster(img)
-        out[] <- NA
+        out   	  <- raster(img)
+        out[]     <- NA
         out[complete] <- model$cluster      
         if("filename" %in% names(wrArgs)) out <- writeRaster(out, ...)
     } else {
