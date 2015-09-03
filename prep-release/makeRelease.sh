3d4ccd2cffafce173e6013718be2d1904d85a527
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-09-03 15:58:37 +0200

    Automatic commit: Pull example data from branch example-data

--- prep-release/makeRelease.sh
+++ prep-release/makeRelease.sh
@@ -17,21 +17,30 @@ echo "**********************************************************"
 Rscript -e "library(devtools);library(methods); document(); install()"
 
 echo "\n**********************************************************"
-echo "Generate example data ************************************"
-echo "**********************************************************"
-Rscript data-raw/generate_data.R
-
-echo "\n**********************************************************"
 echo "Generate sysdata *****************************************"
 echo "**********************************************************"
 Rscript data-raw/generate_sysdata.R
+git commit -a -m "Automatic commit: Document & Update sysdata"
+
+
+## Re-build example data
+git checkout example-data
+echo "\n**********************************************************"
+echo "Generate example data ************************************"
+echo "**********************************************************"
+Rscript data-raw/generate_data.R
+git commit -a -m "Automatic commit: Update example data (landsat, rlogo, srtm)"
+
+## Back to master
+git checkout master
+git checkout example-data data/rlogo.rda data/srtm.rda inst/external/landsat
+Rscript -e "library(devtools); library(methods); document()"
+git commit -a -m "Automatic commit: Pull example data from branch example-data"
 
 echo "\n**********************************************************"
 echo "R CMD check **********************************************"
 echo "**********************************************************"
 Rscript -e "library(devtools); library(methods); build_win(); check()"
-
-git commit -a -m "Automatic commit: Update sysdata and example data"
 
 
 ## Website
