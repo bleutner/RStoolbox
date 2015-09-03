544d80b63fe85d42fb4deab2da4023a1e513e9d0
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-09-03 15:55:36 +0200

    Automatic commit: Update example data (landsat, rlogo, srtm)

--- prep-release/makeRelease.sh
+++ prep-release/makeRelease.sh
@@ -20,18 +20,26 @@ echo "\n**********************************************************"
 echo "Generate example data ************************************"
 echo "**********************************************************"
 Rscript data-raw/generate_data.R
+git commit -a -m "Automatic commit: Document & Update sysdata"
 
+
+## Re-build example data
+git checkout example-data
 echo "\n**********************************************************"
 echo "Generate sysdata *****************************************"
 echo "**********************************************************"
 Rscript data-raw/generate_sysdata.R
+git commit -a -m "Automatic commit: Update example data (landsat, rlogo, srtm)"
+
+git checkout master
+git checkout example-data data/rlogo.rda data/srtm.rda insst/external/landsat
+Rscript -e "library(devtools); library(methods); document()"
+git commit -a -m "Automatic commit: Pull example data from branch example-data"
 
 echo "\n**********************************************************"
 echo "R CMD check **********************************************"
 echo "**********************************************************"
 Rscript -e "library(devtools); library(methods); build_win(); check()"
-
-git commit -a -m "Automatic commit: Update sysdata and example data"
 
 
 ## Website
