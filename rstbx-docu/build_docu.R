daeeb2db8456c1258018de07935cc38da96f74b0
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-09-04 18:47:03 +0200

    tidy html

--- rstbx-docu/build_docu.R
+++ rstbx-docu/build_docu.R
@@ -1,12 +1,13 @@
 library(devtools)
 library(stringr)
- library(knitr)
-   
+library(knitr)
+
 knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = FALSE, cdr = FALSE, copycss=FALSE) {
-   opts_chunk$set(comment="#>")
+    opts_chunk$set(comment="#>")
     force(oldworki <- getwd())
     setwd(path) ; on.exit(setwd(oldworki))
     library(pkg, character.only = TRUE)
+    
     optc = opts_chunk$get(); on.exit(opts_chunk$set(optc), add=T)
     if(copycss) file.copy(system.file('misc', 'R.css', package = 'knitr'), './')
     pkgRdDB = getFromNamespace('fetchRdDB', 'tools')(file.path(find.package(pkg), 'help', pkg))
@@ -37,11 +38,13 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
                 length(grep('</pre>', txt[i:length(txt)]))) {
             i0 = grep('<pre>', txt); i0 = i0[i0 > i][1L] - 1L
             i1 = grep('</pre>', txt); i1 = i1[i1 > i0][1L] + 1L
+            
             tools::Rd2ex(pkgRdDB[[p]], ef <- tempfile(), commentDontrun=cdr)
             ex = readLines(ef, warn = FALSE)
             ex = ex[-(1L:grep('### ** Examples', ex, fixed = TRUE))]
             ex = c('```{r}', ex, '```')
-            opts_chunk$set(fig.path = str_c('figure/', p, '-'), tidy = FALSE)
+            
+            opts_chunk$set(fig.path = str_c('figure/', p, '-'),  tidy = FALSE)
             res = try(knit2html(text = ex, envir = parent.frame(2), fragment.only = TRUE, quiet = TRUE))
             if (inherits(res, 'try-error')) {
                 res = ex; res[1] = '<pre><code class="r">'; res[length(res)] = '</code></pre>'
@@ -52,20 +55,48 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
                             <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/highlight.min.js"></script>
                             <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/languages/r.min.js"></script>
                             <script>hljs.initHighlightingOnLoad();</script>
-                            </head>', txt)
+                            </head>', txt)            
         } else message('no examples found for ', p)
         title <- txt[grep("<head><title>", txt) ]
         title <- gsub("^.*<head><title>R: |</title>", "", title)
         
         H <- grep("<table width=|<h3>Description", txt)
         txt <- txt[-(H[1]:(H[2]-1))] 
-       
+        
         ## Fix bottom index link
         indlink <- grep(">Index</a>", txt)
         txt[indlink] <- gsub("00Index.html", "index.html", txt[indlink])
         
+        ## Remove dontshow sections
+        nosho <- grep("## Don&#39;t show:", txt)
+        if(length(nosho)){
+            su <- txt[nosho]
+            fro <- str_locate( su, "\\n\\n## Don&#39;t show:")
+            fre <- str_locate( su, "## End\\(Don&#39;t show\\)")
+            for(i in 1:nrow(fre)){
+               su <- paste0(substr(su, 1, fro[i,1]-1), substr(su, fre[i,2]+1, nchar(su))) 
+            }
+            txt[nosho] <- su
+        }
+        
+        ## Tidy up don't run section
+        nosho <- grep("\\#\\# Not run:", txt)
+        if(length(nosho)){
+            su <- txt[nosho]
+            fro <- str_locate( su, "\\#\\# Not run: \\n\\#\\#D")
+            fre <- str_locate( su, "\\#\\# End\\(Not run\\)")
+            for(i in 1:nrow(fre)){             
+                substr(su, fro[i,2], fro[i,2])<-" "
+            }
+            txt[nosho] <- su
+        }
+        
+         
+        ## Concatenate lines
         txt <- paste("---\nlayout: docu\ntitle: '",title,"'\nfun: ", p ,"\npackage: ", pkg,
                 "\nheader: Pages\ngroup: navigation\n---\n{% include JB/setup %}\n", paste0(txt, collapse ="\n"))
+        
+        
         writeLines(txt, str_c(p, '.html'))
     }
     unlink('figure/', recursive = TRUE)
