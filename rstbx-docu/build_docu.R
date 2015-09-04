576c675bb64bfd9ea967b2e1838d2957f28eed4f
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-09-04 20:05:50 +0200

    docu rendering

--- rstbx-docu/build_docu.R
+++ rstbx-docu/build_docu.R
@@ -1,6 +1,9 @@
+library(methods)
 library(devtools)
 library(stringr)
 library(knitr)
+library(raster)
+library(RStoolbox)
 
 knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = FALSE, cdr = FALSE, copycss=FALSE) {
     opts_chunk$set(comment="#>")
@@ -11,11 +14,12 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
     optc = opts_chunk$get(); on.exit(opts_chunk$set(optc), add=T)
     if(copycss) file.copy(system.file('misc', 'R.css', package = 'knitr'), './')
     pkgRdDB = getFromNamespace('fetchRdDB', 'tools')(file.path(find.package(pkg), 'help', pkg))
+    
     force(links); topics = names(pkgRdDB)
-    for (p in topics) {
-        message('** knitting documentation of ', p)
-        tools::Rd2HTML(pkgRdDB[[p]], f <- tempfile(),
-                package = pkg, Links = links, no_links = is.null(links), stages = 'render', commentDontrun=cdr)
+    for (topici in topics) { 
+        message('** knitting documentation of ', topici)
+        tools::Rd2HTML(pkgRdDB[[topici]], f <- tempfile(),
+                package = pkg, Links = links, no_links = is.null(links), stages = 'render')
         txt = readLines(f, warn = FALSE)
         extlinks <- grep("^.*\\.\\./\\.\\./", txt)
         
@@ -39,12 +43,16 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
             i0 = grep('<pre>', txt); i0 = i0[i0 > i][1L] - 1L
             i1 = grep('</pre>', txt); i1 = i1[i1 > i0][1L] + 1L
             
-            tools::Rd2ex(pkgRdDB[[p]], ef <- tempfile(), commentDontrun=cdr)
+            tools::Rd2ex(pkgRdDB[[topici]], ef <- tempfile(), commentDontrun=cdr)
             ex = readLines(ef, warn = FALSE)
             ex = ex[-(1L:grep('### ** Examples', ex, fixed = TRUE))]
             ex = c('```{r}', ex, '```')
             
-            opts_chunk$set(fig.path = str_c('figure/', p, '-'),  tidy = FALSE)
+           
+            noru <- grep("##D", ex)
+            ex[noru] <- gsub("##D", "##", ex[noru])
+            
+            opts_chunk$set(fig.path = str_c('figure/', topici, '-'),  tidy = FALSE)
             res = try(knit2html(text = ex, envir = parent.frame(2), fragment.only = TRUE, quiet = TRUE))
             if (inherits(res, 'try-error')) {
                 res = ex; res[1] = '<pre><code class="r">'; res[length(res)] = '</code></pre>'
@@ -56,7 +64,7 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
                             <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/languages/r.min.js"></script>
                             <script>hljs.initHighlightingOnLoad();</script>
                             </head>', txt)            
-        } else message('no examples found for ', p)
+        } else message('no examples found for ', topici)
         title <- txt[grep("<head><title>", txt) ]
         title <- gsub("^.*<head><title>R: |</title>", "", title)
         
@@ -80,24 +88,24 @@ knit_rd2 <- function(pkg, path = ".", links =  tools::findHTMLlinks(), frame = F
         }
         
         ## Tidy up don't run section
-        nosho <- grep("\\#\\# Not run:", txt)
-        if(length(nosho)){
-            su <- txt[nosho]
-            fro <- str_locate( su, "\\#\\# Not run: \\n\\#\\#D")
-            fre <- str_locate( su, "\\#\\# End\\(Not run\\)")
-            for(i in 1:nrow(fre)){             
-                substr(su, fro[i,2], fro[i,2])<-" "
-            }
-            txt[nosho] <- su
-        }
+#        nosho <- grep("\\#\\# Not run:", txt)
+#        if(length(nosho)){
+#            su <- txt[nosho]
+#            fro <- str_locate( su, "\\#\\# Not run: \\n\\#\\#D")
+#            fre <- str_locate( su, "\\#\\# End\\(Not run\\)")
+#            for(i in 1:nrow(fre)){             
+#                substr(su, fro[i,2], fro[i,2])<-" "
+#            }
+#            txt[nosho] <- su
+#        }
         
          
         ## Concatenate lines
-        txt <- paste("---\nlayout: docu\ntitle: '",title,"'\nfun: ", p ,"\npackage: ", pkg,
+        txt <- paste("---\nlayout: docu\ntitle: '",title,"'\nfun: ", topici ,"\npackage: ", pkg,
                 "\nheader: Pages\ngroup: navigation\n---\n{% include JB/setup %}\n", paste0(txt, collapse ="\n"))
         
         
-        writeLines(txt, str_c(p, '.html'))
+        writeLines(txt, str_c(topici, '.html'))
     }
     unlink('figure/', recursive = TRUE)
     toc = sprintf('- <a href="%s" target="content">%s</a>', str_c(topics, '.html'), topics)
