0e867702599d25cc5b2a4b0785feb42dc41ebd09
bleutner <benjamin.leutner@uni-wuerzburg.de>
2015-09-07 20:24:03 +0200

    maybe fix UBSAN issue

--- src/classQA.cpp
+++ src/classQA.cpp
@@ -5,7 +5,7 @@ IntegerVector classQA(NumericVector& x, NumericMatrix rcl){
     int xs = x.size();
     int rclr = rcl.nrow();
     IntegerVector out(xs);
-    std::fill(out.begin(), out.end(), NumericVector::get_na() );
+    std::fill(out.begin(), out.end(), IntegerVector::get_na() );
     for(int r = 0; r < rclr; r++){
           for(int j = 0; j < xs; j++){
             if(rcl(r,0) == x[j]) out[j] = rcl(r,1);
