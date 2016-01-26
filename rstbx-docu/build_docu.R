883c16616c5fea78ef178568c1724579f9984c7b
bleutner <benjamin.leutner@uni-wuerzburg.de>
2016-01-26 19:35:26 +0100

    prep CRAN release

--- rstbx-docu/build_docu.R
+++ rstbx-docu/build_docu.R
@@ -1,4 +1,5 @@
-for(x in c("methods", "devtools", "stringr", "knitr", "caret",
+## Load packages beforehand to avoid cluttering the examples with package startup messages
+for(x in c("methods", "devtools", "stringr", "knitr", "caret","e1071",
         "raster", "rgdal", "randomForest", "pls","grid","gridExtra", 
         "kernlab", "ggplot2", "RStoolbox")){
     suppressMessages(library(x, character.only = TRUE))
