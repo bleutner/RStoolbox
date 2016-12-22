46020cf8d36c369987fa31bb2c26fc9e4fe2e90d
bleutner <benjamin.leutner@uni-wuerzburg.de>
2016-12-22 15:19:57 +0100

    fix band name derivation for Landsat Collection MTL files

--- tests/testthat/test-readMeta.R
+++ tests/testthat/test-readMeta.R
@@ -7,6 +7,7 @@ for(f in mfil) {
 	test_that(paste("readMeta ", basename(f)), {
 				expect_s3_class(m <- readMeta(f), c("RStoolbox", "ImageMetaData"))
 				expect_s3_class(m$ACQUISITION_DATE, c("POSIXlt", "POSIXt")) 
+                expect_true(all(grepl("^B1?[0-9]_(dn|toa|sre){1}$", m$DATA$BANDS[1:4])))
 			})
 	
 }
@@ -14,6 +15,8 @@ for(f in mfil) {
 
 mtlFile  <- system.file("external/landsat/LT52240631988227CUB02_MTL.txt", package="RStoolbox")
 test_that("stackMeta with exampleData", {
-			expect_s4_class(stackMeta(mtlFile), "RasterStack")
+			expect_s4_class(st <- stackMeta(mtlFile), "RasterStack")
 			expect_s4_class(stackMeta(readMeta(mtlFile)), "RasterStack")
+            expect_true(all(grepl("B[1-7]_dn", names(st))))
 		})
+
