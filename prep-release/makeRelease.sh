9d02fe47894258bdcac7a9534899f1a84759c2ef
bleutner <benjamin.leutner@uni-wuerzburg.de>
2017-01-09 16:25:55 +0100

    add rhub tests

--- prep-release/makeRelease.sh
+++ prep-release/makeRelease.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-cd ~/eclipseWorkspace/RStoolbox
+cd ${HOME}/eclipseWorkspace/RStoolbox
 
 ## Check for uncommited changes
-if [[ -n $(git status -s) ]] 
-then
-	echo "Uncommited changes. Clean-up first."
-	git status
-	exit 1
-fi
+function enforce_up2date {
+    git status | grep -q up-to-date
+    status=$? 
+    if [ $status -eq 1 ] 
+    then
+	   echo "Uncommited changes. Clean-up first."
+	   git status
+	   exit 1
+    fi
+}
+
 
 ## Begin build and check
 git checkout master
-echo "\n**********************************************************"
-echo "Document and install RStoolbox ***************************"
-echo "**********************************************************"
-Rscript -e "library(devtools);library(methods); document(); install()"
-
+enforce_up2date
 echo "\n**********************************************************"
 echo "Generate sysdata *****************************************"
 echo "**********************************************************"
-Rscript data-raw/generate_sysdata.R
-git commit -a -m "Automatic commit: Document & Update sysdata"
+statDir=$(git diff --stat HEAD -- data-raw)
+statSysDat=$(git diff --stat HEAD -- R/sysdata.rda)
+if [[ ! -n $statDir ]] || [[ ! -n $statSysDat ]]
+then
+    Rscript data-raw/generate_sysdata.R
+    git commit -a -m "Automatic commit: Document & Update sysdata"
+else
+    echo 'R/sysdata.R is up-to-date'
+fi 
+
+echo "\n**********************************************************"
+echo "Document and install RStoolbox ***************************"
+echo "**********************************************************"
+Rscript -e "library(devtools); library(methods); document(); install()"
 
 
 ## Re-build example data
@@ -37,11 +50,6 @@ git checkout example-data data/rlogo.rda data/srtm.rda  data/lsat.rda inst/exter
 Rscript -e "library(devtools); library(methods); document()"
 git commit -a -m "Automatic commit: Pull example data from branch example-data"
 
-echo "\n**********************************************************"
-echo "R CMD check **********************************************"
-echo "**********************************************************"
-Rscript -e "library(devtools); library(methods);  check(); build_win(version = c('R-release', 'R-devel'))"
-
 ## Website
 echo "\n**********************************************************"
 echo "Build website documentation ******************************"
@@ -50,3 +58,12 @@ git checkout gh-pages
 Rscript rstbx-docu/build_docu.R
 git commit -a -m "Automatic commit: Update gh-pages package documentation"
 git checkout master
+
+echo "\n**********************************************************"
+echo "R CMD check **********************************************"
+echo "**********************************************************"
+#Rscript -e "library(devtools); library(methods);  check(); build_win(version = c('R-release', 'R-devel'))" 
+#Valgrind times out --> run locally
+#Rscript -e "library(rhub); library(methods);  check(platform='debian-gcc-release', valgrind = TRUE)" &> ${HOME}/RHub_RStoolbox_check_with_valgrind.log
+Rscript -e "library(rhub); library(methods);  check(platform=c('debian-gcc-release', 'debian-gcc-devel', 'ubuntu-gcc-devel',  'windows-x86_64-oldrel', 'windows-x86_64-release', 'windows-x86_64-devel','linux-x86_64-rocker-gcc-san'))" &> ${HOME}/RHub_RStoolbox_checks.log
+
