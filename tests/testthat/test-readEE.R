b3ce0ea80676e089181ac945d60b1296cba3a7f2
bleutner <benjamin.leutner@uni-wuerzburg.de>
2017-08-21 20:11:25 +0200

    cleanup

--- tests/testthat/test-readEE.R
+++ tests/testthat/test-readEE.R
@@ -1,10 +1,7 @@
 context("readEE")
 
 exfile <- system.file("external/EarthExplorer_LS8.txt", package = "RStoolbox")
-#tdir <- system.file("testdata/earthexplorer", package="RStoolbox")
 files <- list.files("testdata/earthexplorer", full = TRUE)
-#print(tdir)
-print(files)
 test_that("returned classes", {
 			expect_is(ee <- readEE(files[1]), "data.frame")
             expect_is(ee <- readEE(files), "data.frame")
