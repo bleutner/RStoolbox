1648c85e6f9e66d7390b2cab92bb4fe605c726f1
Benjamin Leutner <benjamin.leutner@uni-wuerzburg.de>
2018-09-20 12:31:47 +0200

    new function oneHotEncode for rasters and vectors

---
+++ R/oneHot.R
@@ -0,0 +1,29 @@
+#' One-hot encode a raster or vector
+#' 
+#' Splits a categorical raster layer (or a vector) into a multilayer raster (or matrix).
+#' 
+#' @param img RasterLayer or integer/numeric vector containing multiple classes
+#' @param classes integer: vector of classes which should be extracted
+#' @param background integer: background value (default = 0)
+#' @param foreground integer: foreground value (default = 1)
+#' @param na.rm logical: if \code{TRUE}, \code{NA}s will be coerced to the \code{background} value.
+#' @param ... further arguments passed to \link[raster]{writeRaster}. Ignored if img is not a RasterLayer, but a numeric/integer vector
+#' @return A RasterBrick with as many layers as there are classes. 
+#' Pixels matching the class of interest are set to 1, backround values by default are set to 0 (see background argument)
+#' @export 
+#' @examples 
+#' data(rlogo)
+#' sc <- unsuperClass(rlogo, nClasses = 3)
+#' sc_oneHot <- oneHotEncode(sc$map)
+#' plot(sc)
+oneHotEncode <- function(img, classes, background = 0, foreground = 1, na.rm = FALSE, ...) {
+    stopifnot(inherits(img, c("RasterLayer", "integer", "numeric")))
+    if(inherits(img, "RasterLayer")) {
+        out <- calc(img, function(x, cl = classes, bg = background, fg = foreground, na.rm) oneHotCpp(x, cl, bg, fg, na.rm), na.rm = na.rm, forcefun = TRUE, ...)
+        names(out) <- paste0("c_", classes)
+    } else {
+        out <- oneHotCpp(img, classes, background, foreground, na_rm = na.rm)
+        colnames(out) <- paste0("c_", classes)
+    }
+    out
+}
