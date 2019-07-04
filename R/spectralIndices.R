590f4f6ab01560ef52f9070f3f66457a54dc5b47
Benjamin Leutner <benjamin.leutner@dlr.de>
2019-07-04 16:44:24 +0200

    add spectral indices

--- R/spectralIndices.R
+++ R/spectralIndices.R
@@ -6,8 +6,11 @@
 #' @param blue Character or integer. Blue band. 
 #' @param green Character or integer. Green band. 
 #' @param red Character or integer. Red band. 
-#' @param nir Character or integer. Near-infrared band (700-1100nm). 
-#' @param swir1 [temporarily deprecated]
+#' @param nir Character or integer. Near-infrared band (700-1100nm).
+#' @param redEdge1 Character or integer. Red-edge band (705nm)
+#' @param redEdge2 Character or integer. Red-edge band (740nm)
+#' @param redEdge3 Character or integer. Red-edge band (783nm)
+#' @param swir1 not used
 #' @param swir2 Character or integer. Short-wave-infrared band (1400-1800nm). 
 #' @param swir3 Character or integer. Short-wave-infrared band (2000-2500nm). 
 #' @param indices Character. One or more spectral indices to calculate (see Details). By default (NULL) all implemented indices given the spectral bands which are provided will be calculated.
@@ -41,7 +44,7 @@
 #' SI <- spectralIndices(lsat_ref, red = "B3_tre", nir = "B4_tre")
 #' plot(SI)
 spectralIndices <- function(img,
-        blue=NULL, green=NULL, red=NULL, nir = NULL, swir1 = NULL, swir2 =NULL, swir3 = NULL,
+        blue=NULL, green=NULL, red=NULL, nir = NULL, redEdge1 = NULL, redEdge2 = NULL, redEdge3 = NULL, swir1 = NULL, swir2 =NULL, swir3 = NULL,
         scaleFactor = 1, skipRefCheck = FALSE,   indices=NULL, index = NULL, maskLayer = NULL, maskValue = 1,
         coefs = list(L = 0.5,  G = 2.5, L_evi = 1,  C1 = 6,  C2 = 7.5, s = 1, swir2ccc = NULL, swir2coc = NULL),
         ... ) {
@@ -49,9 +52,12 @@ spectralIndices <- function(img,
     
     ## We will use the following wavlength range definitions (following Schowengerdt 2007, p 10)
     # VIS   | Visible              |   400  -    700 nm
-    # RED   |
+    # BLUE   |
     # GREEN |
-    # BLUE  |
+    # RED  |
+	# redEdge1
+	# redEdge2
+	# redEdge3
     # NIR   | Near infra-red       |   700  -   1100 nm
     # swir1 | Short-wave infra-red |  1100  -   1351 nm
     # swir2 | shortwave infra-red  |  1400  -   1800 nm
@@ -77,9 +83,9 @@ spectralIndices <- function(img,
     coefs <- c(coefs, defaultCoefs[setdiff(names(defaultCoefs), names(coefs))])
     
     ## Check indices
-    ind <- if(is.null(indices)) names(BANDSdb) else toupper(indices)  
+    ind <- if(is.null(indices)) names(BANDSdb) else indices <- toupper(indices)  
     if((is.null(coefs$swir2ccc) | is.null(coefs$swir2coc))) {
-        if(!is.null(indices) & ("NDVIC" %in% ind)) warning("NDVIc can only be calculated if swir2ccc and swir2coc coefficients are provided.")  
+        if(!is.null(indices) & ("NDVIC" %in% ind)) warning("NDVIc can only be calculated if swir2ccc and swir2coc coefficients are provided.", call. = FALSE)  
         coefs$swir2ccc <- 0  ## dummy, cant pass NULL to spectralIndicesCpp
         coefs$swir2coc <- 1  ## dummy, cant pass NULL to spectralIndicesCpp
         ind <- setdiff(ind, "NDVIC")
@@ -87,7 +93,7 @@ spectralIndices <- function(img,
     if(!any(ind %in% names(BANDSdb))) stop("indices must either be NULL to calculate all indices",
                 "\nor element of c(", paste0(names(BANDSdb),collapse=","),") for specific indices.", call. = FALSE)
     coefs$swir2cdiff <- coefs$swir2coc - coefs$swir2ccc 
-    if(coefs$swir2cdiff <= 0) stop("NDVIc coefficient swir2ccc (completeley closed canopy) must be smaller than swir2coc (completely open canopy)")
+    if(coefs$swir2cdiff <= 0) stop("NDVIc coefficient swir2ccc (completeley closed canopy) must be smaller than swir2coc (completely open canopy)", call. = FALSE)
     
     ## Gather function arguments (all provided bands) and create args
     potArgs  <- c("blue", "green", "red", "nir", "swir1", "swir2", "swir3")
@@ -106,7 +112,7 @@ spectralIndices <- function(img,
             warning("EVI/EVI2 parameters L_evi, G, C1 and C2 are defined for reflectance [0,1] but img values are outside of this range.\n",
                     "  If you are using scaled reflectance values please provide the scaleFactor argument.\n", 
                     "  If img is in DN or radiance it must be converted to reflectance.\n",
-                    "  Skipping EVI calculation.\n")
+                    "  Skipping EVI calculation.\n", call. = FALSE)
             canCalc <- canCalc[!canCalc %in% c("EVI", "EVI2")]
             indices <- indices[!indices %in% c("EVI", "EVI2")]
         }
@@ -115,14 +121,14 @@ spectralIndices <- function(img,
     ind  <- ind[ind %in% canCalc]   
     
     if(!length(ind)) stop("No index could be calculated. At least for one index you must specify *all* required bands.",
-                "\n  See ?spectralIndices for information on required bands per index.")
+                "\n  See ?spectralIndices for information on required bands per index.", call. = FALSE)
     if(length(ind) < length(indices)){
         not <- setdiff(indices,ind)
         notbands <- setdiff(unlist(requested[not]), bands)
-        warning("The following indices were requested but cannot be calculated: ", not,
+        warning("The following indices were requested but cannot be calculated: ", paste(not, collapse = ", "),
                 "\n  because the following required bands were not specified: ",
-                notbands,
-                "\n  The remaining fully specified indices will be calculated.")
+                paste(notbands, collapse = ", "),
+                "\n  The remaining fully specified indices will be calculated.", call. = FALSE)
     }
     
     ## Get required designated bands
@@ -164,6 +170,9 @@ spectralIndices <- function(img,
                                 blueBand  = fullSet[["blue"]],
                                 greenBand = fullSet[["green"]], 
                                 redBand   = fullSet[["red"]],
+								redEdgeBand1 =  fullSet[["redEdge1"]],
+								redEdgeBand2 =  fullSet[["redEdge2"]],
+								redEdgeBand3 =  fullSet[["redEdge3"]],
                                 nirBand   = fullSet[["nir"]], 
                                 swir1Band  = fullSet[["swir1"]],                             
                                 swir2Band  = fullSet[["swir2"]],
@@ -190,24 +199,31 @@ spectralIndices <- function(img,
 #' @noRd 
 .IDXdb <-  list(     
         CTVI    = function(red, nir) {(NDVI+.5)/sqrt(abs(NDVI+.5))},
+		CLG     = function(redEdge3, green) {redEdge3/green - 1}, 
+		CLRE    = function(redEdge3, redEdge1) {redEdge3/redEdge1 - 1}, 
         DVI     = function(red, nir) {s*nir-red},
-        EVI      = function(red, nir, blue) {G * ((nir - red) / (nir + C1 * red - C2 * blue + L_evi))},
+        EVI     = function(red, nir, blue) {G * ((nir - red) / (nir + C1 * red - C2 * blue + L_evi))},
         EVI2    = function(red, nir) {G * (nir-red)/(nir + 2.4*red +1)},
         GEMI    = function(red, nir) {(((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * (1 - ((((nir^2 - red^2) * 2 + (nir * 1.5) + (red * 0.5) ) / (nir + red + 0.5)) * 0.25)) - ((red - 0.125) / (1 - red))},
-        GNDVI    = function(green, nir) {(nir-green)/(nir+green)}, 
+        GNDVI   = function(green, nir) {(nir-green)/(nir+green)}, 
         MNDWI   = function(green, swir2) {(green-swir2) / (green+swir2)},
-        MSAVI    = function(red, nir) {nir + 0.5 - (0.5 * sqrt((2 * nir + 1)^2 - 8 * (nir - (2 * red))))},
-        MSAVI2    = function(red, nir) {(2 * (nir + 1) - sqrt((2 * nir + 1)^2 - 8 * (nir - red))) / 2},
+		MCARI   = function(green, red, redEdge1) {((redEdge1-red)-(redEdge1-green))*(redEdge1/red)},
+		MTCI    = function(red, redEdge1, redEdge2) {(redEdge2-redEdge1)/(redEdge1-red)},
+        MSAVI   = function(red, nir) {nir + 0.5 - (0.5 * sqrt((2 * nir + 1)^2 - 8 * (nir - (2 * red))))},
+        MSAVI2  = function(red, nir) {(2 * (nir + 1) - sqrt((2 * nir + 1)^2 - 8 * (nir - red))) / 2},
         NBRI    = function(nir, swir3) { (nir - swir3) / (nir + swir3)},
-        NDVI    = function(red, nir) {(nir-red)/(nir+red)}, 
+		NDREI1  = function(redEdge2, redEdge1) {(redEdge2-redEdge1)/(redEdge2+redEdge1)}, 
+		NDREI2  = function(redEdge3, redEdge1) {(redEdge3-redEdge1)/(redEdge3+redEdge1)}, 
+		NDVI    = function(red, nir) {(nir-red)/(nir+red)}, 
         NDVIC   = function(red, nir, swir2) {(nir-red)/(nir+red)*(1-((swir2 - swir2ccc)/(swir2coc-swir2ccc)))},
-        NDWI     = function(green, nir) {(green - nir)/(green + nir)},
-        NDWI2    = function(nir, swir2) {(nir - swir2)/(nir + swir2)},    
+        NDWI    = function(green, nir) {(green - nir)/(green + nir)},
+        NDWI2   = function(nir, swir2) {(nir - swir2)/(nir + swir2)},    
         NRVI    = function(red, nir) {(red/nir - 1)/(red/nir + 1)},
-        RVI     = function(red, nir) {red/nir},
+        REIP    = function(red, redEdge1, redEdge2, redEdge3) {0.705+0.35*((red+redEdge3)/(2-redEdge1))/(redEdge2-redEdge1)},
+		RVI     = function(red, nir) {red/nir},
         SATVI   = function(red, swir2, swir3) {(swir2 - red) / (swir2 + red + L) * (1 + L) - (swir3 / 2)},
         SAVI    = function(red, nir) {(nir - red) * (1+L) / (nir + red + L)}, 
-        SLAVI    = function(red, nir, swir2) {nir / (red + swir2)},
+        SLAVI   = function(red, nir, swir2) {nir / (red + swir2)},
         SR      = function(red, nir) {nir / red},     
         TVI     = function(red, nir) {sqrt((nir-red)/(nir+red)+0.5)},
         TTVI    = function(red, nir) {sqrt(abs((nir-red)/(nir+red) + 0.5))},
@@ -223,23 +239,30 @@ BANDSdb <- lapply(.IDXdb, function(x) names(formals(x)))
 .IDX.REFdb <- list(
         DVI     = c("Richardson1977", "Difference Vegetation Index") ,
         CTVI    = c("Perry1984", "Corrected Transformed Vegetation Index"),
+		CLG     = c("Gitelson2003", "Green-band Chlorophyll Index"),
+		CLRE    = c("Gitelson2003", "Red-edge-band Chlorophyll Index"),
         EVI     = c("Huete1999", "Enhanced Vegetation Index"),
         EVI2    = c("Jiang 2008", "Two-band Enhanced Vegetation Index"), # Development of a two-band enhanced vegetation index without a blue band
         GEMI    = c("Pinty1992","Global Environmental Monitoring Index"),
-        GNDVI    = c("Gitelson1998", "Green Normalised Difference Vegetation Index"),
-        MSAVI    = c("Qi1994","Modified Soil Adjusted Vegetation Index"),
-        MSAVI2    = c("Qi1994","Modified Soil Adjusted Vegetation Index 2"),
-        MNDWI   = c("Xu2006", "Modified Normalised Difference Water Index"),       
+        GNDVI   = c("Gitelson1998", "Green Normalised Difference Vegetation Index"),
+        MSAVI   = c("Qi1994","Modified Soil Adjusted Vegetation Index"),
+        MSAVI2  = c("Qi1994","Modified Soil Adjusted Vegetation Index 2"),
+        MNDWI   = c("Xu2006", "Modified Normalised Difference Water Index"),  
+		MCARI   = c("Daughtery2000", "Modified Chlorophyll Absorption Ratio Index"),  
+		MTCI    = c("DashAndCurran2004", "MERIS Terrestrial Chlorophyll Index"),
         NBRI    = c("Garcia1991", "Normalised Burn Ratio Index"),
+		NDREI1  = c("GitelsonAndMerzlyak1994", "Normalised Difference Red Edge Index 1"),
+		NDREI2  = c("Barnes2000", "Normalised Difference Red Edge Index 2"),
         NDVI    = c("Rouse1974", "Normalised Difference Vegetation Index"),
         NDVIC   = c("Nemani1993", "Corrected Normalised Difference Vegetation Index"),
         NDWI    = c("McFeeters1996", "Normalised Difference Water Index"), # The use of the Normalized Difference Water Index (NDWI) in the delineation of open water features
-        NDWI2    = c("Gao1996", "Normalised Difference Water Index"),
+        NDWI2   = c("Gao1996", "Normalised Difference Water Index"),
         NRVI    = c("Baret1991","Normalised Ratio Vegetation Index"),
         RVI     = c("", "Ratio Vegetation Index"),
+		REIP    = c("GuyotAndBarnet1988", "Red Edge Inflection Point"),
         SATVI   = c("Marsett2006", "Soil Adjusted Total Vegetation Index"),
         SAVI    = c("Huete1988", "Soil Adjusted Vegetation Index"),
-        SLAVI    = c("Lymburger2000","Specific Leaf Area Vegetation Index"),
+        SLAVI   = c("Lymburger2000","Specific Leaf Area Vegetation Index"),
         SR      = c("Birth1968", "Simple Ratio Vegetation Index"),  #or Jordan1969
         TVI     = c("Deering1975","Transformed Vegetation Index"),
         TTVI    = c("Thiam1997", "Thiam's Transformed Vegetation Index"),
--- src/spectralIndices.cpp
+++ src/spectralIndices.cpp
@@ -4,51 +4,55 @@ using namespace Rcpp;
 
 //[[Rcpp::export]]
 NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
-		 const int redBand,  const int blueBand, const int greenBand, const int nirBand,
-		 const int swir1Band, const int swir2Band, const int swir3Band,
-		 int maskLayer, const int maskValue,
-		 const double L,  const double s, const double G, const double C1,
-		 const double C2, double Levi, const double swir2ccc, const double swir2cdiff, const double sf) {
+		const int redBand,  const int blueBand, const int greenBand, const int nirBand,
+		const int redEdge1Band, const int redEdge2Band,  const int redEdge3Band,
+		const int swir1Band, const int swir2Band, const int swir3Band,
+		int maskLayer, const int maskValue,
+		const double L,  const double s, const double G, const double C1,
+		const double C2, double Levi, const double swir2ccc, const double swir2cdiff, const double sf) {
 
 	int nind = indices.size();
 	int nsamp = x.nrow();
 
 	NumericMatrix out(nsamp, nind);
-	NumericVector blue, green, red, nir, swir1, swir2, swir3;
-    
-    // Apply mask layer
-    if(maskLayer != NA_INTEGER){
-        maskLayer-=1 ;
-        int nx = x.nrow();
-        std::vector<int> m;
-        m.reserve(nx);
-        if(IntegerVector::is_na(maskValue)){
-            for(int i = 0; i < nx; i++) {
-                if (ISNAN(x(i, maskLayer))) m.push_back(i);
-            }
-        } else {
-            for(int i = 0; i < nx; i++) {
-                if (x(i, maskLayer) == maskValue) m.push_back(i);
-            }
-        } 
-        
-        for(int j = 0; j < x.ncol(); j++) { 
-            if (j == maskLayer) continue;
-            for(int i = 0; i < m.size(); i++) {
-                 x(m[i],j) = NA_REAL;
-            }    
-         }
-    }
+	NumericVector blue, green, red, redEdge1, redEdge2, redEdge3, nir, swir1, swir2, swir3;
+
+	// Apply mask layer
+	if(maskLayer != NA_INTEGER){
+		maskLayer-=1 ;
+		int nx = x.nrow();
+		std::vector<int> m;
+		m.reserve(nx);
+		if(IntegerVector::is_na(maskValue)){
+			for(int i = 0; i < nx; i++) {
+				if (ISNAN(x(i, maskLayer))) m.push_back(i);
+			}
+		} else {
+			for(int i = 0; i < nx; i++) {
+				if (x(i, maskLayer) == maskValue) m.push_back(i);
+			}
+		}
+
+		for(int j = 0; j < x.ncol(); j++) {
+			if (j == maskLayer) continue;
+			for(int i = 0; i < m.size(); i++) {
+				x(m[i],j) = NA_REAL;
+			}
+		}
+	}
 
 	if(blueBand  != NA_INTEGER)     blue = x(_,blueBand - 1) / sf;
 	if(greenBand != NA_INTEGER)    green = x(_,greenBand - 1) / sf;
 	if(redBand   != NA_INTEGER)      red = x(_,redBand - 1) / sf;
+	if(redEdge1Band != NA_INTEGER) redEdge1 = x(_,redEdge1Band - 1) / sf;
+	if(redEdge2Band != NA_INTEGER) redEdge2 = x(_,redEdge2Band - 1) / sf;
+	if(redEdge3Band != NA_INTEGER) redEdge3 = x(_,redEdge3Band - 1) / sf;
 	if(nirBand   != NA_INTEGER)      nir = x(_,nirBand - 1) / sf;
 	if(swir1Band != NA_INTEGER)    swir1 = x(_,swir1Band - 1) / sf;
 	if(swir2Band != NA_INTEGER)    swir2 = x(_,swir2Band - 1) / sf;
 	if(swir3Band != NA_INTEGER)    swir3 = x(_,swir3Band - 1) / sf;
 
-   
+
 
 	for(int j = 0; j < nind; ++j) {
 
@@ -64,6 +68,18 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = np / sqrt(abs(np));
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}
+		else if(indices[j] == "CLG") {
+			// Green cholorphyl index
+			// Wu et al 2012
+			out(_,j) = (redEdge3)/(green)-1;
+			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "CLRE") {
+			// RedEdge cholorphyl index
+			// Clevers and Gitelson 2013
+			out(_,j) = (redEdge3)/(redEdge1)-1;
+			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
+		}
 		else if(indices[j] == "EVI") {
 			// Enhanced vegetation index
 			// Huete et al 1990
@@ -71,12 +87,11 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "EVI2") {
-				// Two-band Enhanced vegetation index
-				// Jiang et al 2008
-				out(_,j) = G * ((nir - red) / (nir + 2.4 * red ));
-				out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
-			}
-
+			// Two-band Enhanced vegetation index
+			// Jiang et al 2008
+			out(_,j) = G * ((nir - red) / (nir + 2.4 * red ));
+			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
+		}
 		else if(indices[j] == "GEMI") {
 			out(_,j) = (((pow(nir, 2) - pow(red, 2)) * 2.0 + (nir * 1.5) + (red * 0.5) ) /
 					(nir + red + 0.5)) * (1 - ((((pow(nir,2) - pow(red,2)) * 2 + (nir * 1.5) + (red * 0.5) ) /
@@ -84,15 +99,27 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}	
 		else if(indices[j] == "GNDVI") {
-					// green Normalized diff vegetation index: -> more sensitive to cholorphyll than ndvi
-					// Gitelson, A., and M. Merzlyak. "Remote Sensing of Chlorophyll Concentration in Higher Plant Leaves." Advances in Space Research 22 (1998): 689-692
-					out(_,j) = (nir - green)/( nir + green);
-					out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
-				}
+			// green Normalized diff vegetation index: -> more sensitive to cholorphyll than ndvi
+			// Gitelson, A., and M. Merzlyak. "Remote Sensing of Chlorophyll Concentration in Higher Plant Leaves." Advances in Space Research 22 (1998): 689-692
+			out(_,j) = (nir - green)/( nir + green);
+			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
+		}
 		else if(indices[j] == "MNDWI") {
-            // Modified Normalised Difference Water Index
+			// Modified Normalised Difference Water Index
 			out(_,j) = (green-swir2) / (green+swir2);
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "MTCI") {
+			// Meris Terrestrial Chlorophyll Index
+			// Clevers and Gitelson 2013, Dash and Curran 2004
+			out(_,j) = (redEdge2-redEdge1) / (redEdge1-red);
+			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "MCARI") {
+			// Modified Chlorophyll Absorption ratio index
+			// Daughtery et al. 2000
+			out(_,j) = (redEdge1 - red - redEdge1 + green) * (redEdge1 / red);
+			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "MSAVI") {
 			// Modified soil adjusted vegetation index
@@ -100,17 +127,17 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "MSAVI2") {
-		    // Modified soil adjusted vegetation index 2
+			// Modified soil adjusted vegetation index 2
 			out(_,j) = (2.0 * nir + 1.0 - sqrt(pow(2.0 * nir + 1.0, 2) - 8.0 * (nir - red))) / 2.0;
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
-	   }
-	   else if(indices[j] == "NDVIC") {
+		}
+		else if(indices[j] == "NDVIC") {
 			//Normalized difference vegetation index
 			out(_,j) = (nir - red) / (nir + red) * (1 - (swir2 - swir2ccc)/swir2cdiff);
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0), NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "NBRI"){
-            // Normalised Burn Ratio Index
+			// Normalised Burn Ratio Index
 			out(_,j) = (nir - swir3) / (nir + swir3);
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0), NA_REAL, out(_,j));
 		}
@@ -126,17 +153,35 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "NDWI2") {
-			// Normalized difference water index
+			// Normalized difference water index (also known as Normalized Difference Moisture Index (NDBI)
 			// Gao 1996, Chen 2005
 			// a.k.a. LSWI
 			out(_,j) = (nir - swir2)/(nir + swir2);
 			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
 		}
-	    else if(indices[j] == "NRVI") {
+		else if(indices[j] == "NDREI1") {
+			// Normalized difference red edge index
+			// Gitelson and Merzlyak 1994
+			out(_,j) = (redEdge2 - redEdge1)/(redEdge2 + redEdge1);
+			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "NDREI2") {
+			// Normalized difference red edge index 2
+			// Barnes et al 2000
+			out(_,j) = (redEdge3 - redEdge1)/(redEdge3 + redEdge1);
+			out(_,j) = ifelse(is_na(out(_,j)) | (out(_,j) > 1.0) | (out(_,j) < -1.0) , NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "NRVI") {
 			// Normalized Ratio Vegetation Index
 			// Baret and Guyot 1991
 			NumericVector rvi = red / nir;
 			out(_,j) = (rvi - 1.0)/(rvi + 1.0);
+			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
+		}
+		else if(indices[j] == "REIP") {
+			// Ratio Vegetation Index
+			// Richardson and Wiegand 1977
+			out(_,j) = 0.705+0.35*((red+redEdge3)/(2-redEdge1))/(redEdge2-redEdge1) ;
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}
 		else if(indices[j] == "RVI") {
@@ -145,12 +190,11 @@ NumericMatrix spectralIndicesCpp(NumericMatrix x, CharacterVector indices,
 			out(_,j) = red / nir;
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
 		}
-	
 		else if(indices[j] == "SATVI"){
-            // Soil adjusted total vegetation index
+			// Soil adjusted total vegetation index
 			out(_,j) = ((swir2 - red) / (swir2 + red + L)) * (1.0 + L) - (swir3 / 2.0);
 			out(_,j) = ifelse(is_na(out(_,j)), NA_REAL, out(_,j));
-                }
+		}
 		else if(indices[j] == "SAVI") {
 			// Soil adjusted vegetation index
 			// Huete1988
