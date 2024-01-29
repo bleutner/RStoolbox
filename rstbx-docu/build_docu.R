1e99fb389ad83a936c682620d47503c4104da34c
Konstantin <konstantinfinn.mueller@gmx.de>
2024-01-29 01:41:13 +0100

    test of minimalistic knitr page

--- rstbx-docu/build_docu.R
+++
@@ -1,9 +0,0 @@
- ## Load packages beforehand to avoid cluttering the examples with package startup messages
-for(x in c("methods", "devtools", "stringr", "knitr", "caret", "e1071",
-        "terra", "sf", "randomForest", "pls", "grid", "gridExtra",
-        "kernlab", "ggplot2", "RStoolbox")){
-    suppressMessages(library(x, character.only = TRUE))
-}
-
-knitr::knit_rd(pkg = "RStoolbox")
-
