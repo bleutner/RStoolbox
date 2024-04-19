218fd5fac31e368ccf705a1fa7e2c132ea3fbec6
KonstiDE <konstantin.mueller1@stud-mail.uni-wuerzburg.de>
2024-04-19 14:44:41 +0200

    Remove startup message, sad...

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,6 @@
 .onAttach <- function(libname = find.package("RStoolbox"), pkgname = "RStoolbox") {
   packageStartupMessage("This is version ", utils::packageVersion(pkgname), " of ", pkgname)
 
-  packageStartupMessage("
-        ____       __              ____
-       / __ \\_____/ /_____  ____  / / /_  ____  _  __
-      / /_/ / ___/ __/ __ \\/ __ \\/ / __ \\/ __ \\| |/_/
-     / _, _(__  ) /_/ /_/ / /_/ / / /_/ / /_/ />  <
-    /_/ |_/____/\\__/\\____/\\____/_/_.___/\\____/_/|_|
-  ")
-
   lsat_rs <- terra::readRDS(system.file("external", "lsat.rds", package = pkgname))
   assign("lsat", lsat_rs, envir=as.environment("package:RStoolbox"))
 
